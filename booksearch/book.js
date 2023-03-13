b26008584ac1f74573b0a0ee14fbe5e1f5244a29
gnwlshap <gnwlshap@naver.com>
2023-03-13 14:11:39 +0900

    230313

--- booksearch/book.js
+++ booksearch/book.js
@@ -14,21 +14,44 @@
 // -> jQuery.ajax() 메소드를 활용
 
 let page = 1;
+let size = 20;
 
 const query = document.querySelector(".query");
-
 const searchBox = document.querySelector(".search-box");
+
 searchBox.addEventListener("submit", e =>{
     e.preventDefault();
     if(query !== "") {
         page = 1;
+        setSize(query, page);
         searchRequest(query.value, page);
+        query.value = "";
     }
 })
 
-function searchRequest(query, page) {
+function setSize(query, page) {
     $.ajax({
         "url": `https://dapi.kakao.com/v3/search/book?query=${query}&page=${page}&size=20&target=title`,
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Authorization": "KakaoAK 5e0b5e06e4fb88f8a3728d0c4aa130eb"
+        },
+    })
+    .done((response) => {
+        let result = response.documents;
+        
+        for(let i=0; i<result.length; i++) {
+            if(result[i].thumbnail === ""){
+                size ++;
+            }
+        }
+    })
+}
+
+function searchRequest(query, page) {
+    $.ajax({
+        "url": `https://dapi.kakao.com/v3/search/book?query=${query}&page=${page}&size=${size}&target=title`,
         "method": "GET",
         "timeout": 0,
         "headers": {
@@ -41,51 +64,55 @@ function searchRequest(query, page) {
         const container = document.querySelector(".container");
         container.innerText = ""; 
         let result = response.documents;
-        
         for(let i=0; i<result.length; i++) {
-            const resultCard = document.createElement("div");
-            resultCard.setAttribute("class", "result-card");
+            if(result[i].thumbnail !== ""){
+                const resultCard = document.createElement("div");
+                resultCard.setAttribute("class", "result-card");
+        
+                const bookImg = document.createElement("img");
+                bookImg.setAttribute("class", "book-img");
+                // bookImg.setAttribute("href", result[i].url)
+                bookImg.src = result[i].thumbnail;
+                bookImg.addEventListener("click", e =>{
+                    location.href = result[i].url;
+                })
+                resultCard.appendChild(bookImg);
+        
+                const bookTitle = document.createElement("h4");
+                bookTitle.setAttribute("class", "book-title");
+                bookTitle.innerText = result[i].title;
+                resultCard.appendChild(bookTitle);
+        
+                const price = document.createElement("span");
     
-            const bookImg = document.createElement("img");
-            bookImg.setAttribute("class", "book-img");
-            bookImg.src = result[i].thumbnail;
-            resultCard.appendChild(bookImg);
+                if(result[i].sale_price > 0){
+                    price.setAttribute("class", "price");
+                    price.innerText = result[i].sale_price + "원";
+                }
+                else{
+                    price.setAttribute("class", "zero");
+                    price.innerText = "절판";
+                }
+                resultCard.appendChild(price);
+        
+                const bookInfo = document.createElement("p");
+                bookInfo.setAttribute("class", "book-info");
     
-            const bookTitle = document.createElement("h4");
-            bookTitle.setAttribute("class", "book-title");
-            bookTitle.innerText = result[i].title;
-            resultCard.appendChild(bookTitle);
+                resultCard.appendChild(bookInfo);
     
-            const price = document.createElement("span");
-
-            if(result[i].sale_price > 0){
-                price.setAttribute("class", "price");
-                price.innerText = result[i].sale_price + "원";
+                const author = document.createElement("span");
+                author.setAttribute("class", "author");
+                const publisher = document.createElement("span");
+                publisher.setAttribute("class", "publisher");
+    
+                author.innerText = result[i].authors+" ";
+                publisher.innerText = "| "+result[i].publisher;
+    
+                bookInfo.appendChild(author);
+                bookInfo.appendChild(publisher);
+                
+                container.appendChild(resultCard);
             }
-            else{
-                price.setAttribute("class", "zero");
-                price.innerText = "절판";
-            }
-            resultCard.appendChild(price);
-    
-            const bookInfo = document.createElement("p");
-            bookInfo.setAttribute("class", "book-info");
-
-            resultCard.appendChild(bookInfo);
-
-            const author = document.createElement("span");
-            author.setAttribute("class", "author");
-            const publisher = document.createElement("span");
-            publisher.setAttribute("class", "publisher");
-
-            author.innerText = result[i].authors;
-            publisher.innerText = "|"+result[i].publisher;
-
-            bookInfo.appendChild(author);
-            bookInfo.appendChild(publisher);
-            
-    
-            container.appendChild(resultCard);
         }
         console.log(page);
 
@@ -106,8 +133,7 @@ function searchRequest(query, page) {
 
         pageMove.append(backBtn);
 
-
-        pageMove.append(`${page} / ${Math.ceil(response.meta.pageable_count/20)}`);
+        pageMove.append(`${page} / ${Math.ceil(response.meta.pageable_count/size)}`);
 
         const nextBtn = document.createElement("img");
         
